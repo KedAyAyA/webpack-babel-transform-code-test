30443dabd03b3ee56acfb3bc8c4a2c0a4c6270cd
KedAyAyA <caoxun333@gmail.com>
2018-01-10 10:43:18 +0800

    complete

--- src/index.js
+++ src/index.js
@@ -7,9 +7,27 @@ moduleB.test()
 // var c = moduleB.a
 console.log(moduleA)
 
+// var p = new Promise((resolve, reject) => {
+//   if (Math.random() > 0.5) {
+//     resolve(1)
+//   } else {
+//     reject('error')
+//   }
+// })
+// p.then(num => {
+//   console.log(num)
+// }).catch(e => {
+//   console.log(e)
+// })
+Object.assign({}, {
+  a: 1,
+  b: 'hehe'
+})
+
 export default class A {
   static test () {
     console.log('hehe')
     [1, 2].map(item => item * 2)
+    "123".include('1')
   }
 }
